cc0c7b25a6c1be213a4129e5c124ed5881cfb819
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-02-05 22:27:01 -0300

    Projeto Finalizado

---
+++ src/components/Comment.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/Brunogodoy2911.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Bruno Godoy</strong>
+              <time
+                title="03 de fevereiro às 4:21PM"
+                dateTime="2025-02-03 4:21PM"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp /> Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1668554245893-2430d0077217?w=500&auto=format&fit=crop&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGV2ZWxvcGVyfGVufDB8fDB8fHww"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/Brunogodoy2911.png" />
+
+        <strong>Bruno Godoy</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
